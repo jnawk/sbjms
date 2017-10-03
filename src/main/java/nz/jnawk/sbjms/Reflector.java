0e285507ec7503a63710975a68fe39da9a2d1423
Philip Dowie <philip.dowie@apitalent.co.nz>
2017-10-03 22:56:00 +1300

    added another endpoint

--- src/main/java/nz/jnawk/sbjms/Reflector.java
+++ src/main/java/nz/jnawk/sbjms/Reflector.java
@@ -1,17 +1,24 @@
 package nz.jnawk.sbjms;
 
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListenerConfigurer;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerEndpointRegistrar;
+import org.springframework.jms.config.SimpleJmsListenerEndpoint;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
@@ -19,37 +26,59 @@ import org.springframework.jms.support.converter.MessageType;
 
 @SpringBootApplication
 @EnableJms
-public class Reflector {
+public class Reflector implements JmsListenerConfigurer {
 	private static final Logger logger = LoggerFactory.getLogger(Reflector.class);
 
-	public static void main(String[] args)  {
+	// set this by passing --id=something or by declaring ID environment variable
+	@Value("${ID:foo}") // set to foo if not set
+	private String initialDestination;
+
+	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Reflector.class, args);
 
 		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
 
 		// Send a message with a POJO - the template reuse the message converter
 		logger.info("Sending an email message.");
-		jmsTemplate.convertAndSend("mailbox", "Hello, world");
+		jmsTemplate.convertAndSend(initialDestination, "Hello, world");
 	}
 
+	@Autowired
+	private Receiver receiver;
 
-    @Bean
-    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
-                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        // This provides all boot's default to this factory, including the message converter
-        configurer.configure(factory, connectionFactory);
-        // You could still override some of Boot's default if necessary.
-        return factory;
-    }
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		// This provides all boot's default to this factory, including the message
+		// converter
+		configurer.configure(factory, connectionFactory);
+		// You could still override some of Boot's default if necessary.
+		return factory;
+	}
 
-    @Bean // Serialize message content to json using TextMessage
-    public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        return converter;
-    }
+	@Bean // Serialize message content to json using TextMessage
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
 
+	@Override
+	public void configureJmsListeners(JmsListenerEndpointRegistrar registrar) {
+		SimpleJmsListenerEndpoint endpoint = new SimpleJmsListenerEndpoint();
+		endpoint.setDestination(initialDestination);
+		endpoint.setMessageListener(message -> {
+			try {
+				receiver.receiveMessage(((TextMessage) message).getText());
+			} catch (JMSException e) {
+				// lambdas can't throw checked exceptions
+				throw new RuntimeException(e);
+			}
+		});
+		registrar.registerEndpoint(endpoint);
+
+	}
 
 }
