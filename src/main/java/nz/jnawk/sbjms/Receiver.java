1427a3b52b56798fdd19bb4f10db94fc067b5f52
unknown <aimzoe@gmail.com>
2017-10-03 10:53:25 +0100

    removed sending messages, receiving messages and sending responses only

--- src/main/java/nz/jnawk/sbjms/Receiver.java
+++ src/main/java/nz/jnawk/sbjms/Receiver.java
@@ -2,16 +2,23 @@ package nz.jnawk.sbjms;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 	private static final Logger logger = LoggerFactory.getLogger(Receiver.class);
+	
+	@Autowired
+	private JmsTemplate jmsTemplate;
 
 	@JmsListener(destination = "mailbox", containerFactory = "myFactory")
 	public void receiveMessage(final String content) {
 		logger.info("Received <" + content + ">");
+		
+		jmsTemplate.convertAndSend("mailbox", "Hello, back world!");
 	}
 
 }
--- src/main/java/nz/jnawk/sbjms/Reflector.java
+++ src/main/java/nz/jnawk/sbjms/Reflector.java
@@ -2,17 +2,13 @@ package nz.jnawk.sbjms;
 
 import javax.jms.ConnectionFactory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
@@ -20,36 +16,28 @@ import org.springframework.jms.support.converter.MessageType;
 @SpringBootApplication
 @EnableJms
 public class Reflector {
-	private static final Logger logger = LoggerFactory.getLogger(Reflector.class);
 
-	public static void main(String[] args)  {
-		ConfigurableApplicationContext context = SpringApplication.run(Reflector.class, args);
-
-		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
-
-		// Send a message with a POJO - the template reuse the message converter
-		logger.info("Sending an email message.");
-		jmsTemplate.convertAndSend("mailbox", "Hello, world");
+	public static void main(String[] args) {
+		SpringApplication.run(Reflector.class, args);
 	}
 
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		// This provides all boot's default to this factory, including the message
+		// converter
+		configurer.configure(factory, connectionFactory);
+		// You could still override some of Boot's default if necessary.
+		return factory;
+	}
 
-    @Bean
-    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
-                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        // This provides all boot's default to this factory, including the message converter
-        configurer.configure(factory, connectionFactory);
-        // You could still override some of Boot's default if necessary.
-        return factory;
-    }
-
-    @Bean // Serialize message content to json using TextMessage
-    public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        return converter;
-    }
-
+	@Bean // Serialize message content to json using TextMessage
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
 
 }
