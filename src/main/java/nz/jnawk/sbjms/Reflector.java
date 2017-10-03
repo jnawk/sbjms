3f4a286edcf4f972de56b3e335b115568eafa108
Philip Dowie <philip.dowie@apitalent.co.nz>
2017-10-03 22:22:44 +1300

    annotations

--- src/main/java/nz/jnawk/sbjms/Reflector.java
+++ src/main/java/nz/jnawk/sbjms/Reflector.java
@@ -5,9 +5,11 @@ import javax.jms.ConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
@@ -15,6 +17,8 @@ import org.springframework.jms.support.converter.MappingJackson2MessageConverter
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
+@SpringBootApplication
+@EnableJms
 public class Reflector {
 	private static final Logger logger = LoggerFactory.getLogger(Reflector.class);
 
